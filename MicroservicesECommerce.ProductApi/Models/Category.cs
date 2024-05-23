ef12df968319caf723c82115f5af695788c2853f
Giovani de Oliveira <giovani98@proton.me>
2024-05-23 10:38:16 -0300

    feat: Initialize project

---
+++ MicroservicesECommerce.ProductApi/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MicroservicesECommerce.ProductApi.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+
+    [JsonIgnore]
+    public ICollection<Product>? Products { get; set; }
+}
